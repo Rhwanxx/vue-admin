c2f329514fc919b7197c620727044ce80c013fd7
wanrh <13072760107@163.com>
2020-05-26 15:35:29 +0800

    初始化

---
+++ src/views/login/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="login">
+        <el-button type="primary">主要按钮</el-button>
+    </div>
+</template>
+<script>
+export default {
+    name: "login"
+}
+</script>
+<style lang="scss" scoped>
+#login {
+    height: 100vh;
+    background-color: #344a5f;
+}
+</style>
